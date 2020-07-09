6038ff7014e9af3f799d35cfefdfb09856c4fd96
Jaksa Malisic <jaksa.malisic@gmail.com>
2020-07-09 11:09:18 +0200

    typed crypto

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,5 +1,5 @@
 import { Buffer } from 'buffer'
-import sha3 from 'js-sha3'
+import sha3, { Message } from 'js-sha3'
 import mur from 'murmurhash3js-revisited'
 import { factory as sha } from './sha'
 import { fromNumberTo32BitBuf } from './utils'
@@ -8,7 +8,8 @@ import { fromNumberTo32BitBuf } from './utils'
 // the function as async because it must return a Promise to match the API
 // for other functions that do perform asynchronous work (see sha.browser.js)
 // eslint-disable-next-line
-const hash = (algorithm) => async (data) => {
+type HashAlgorithm = 'sha3-224' | 'sha3-256' | 'sha3-384' | 'sha3-512' | 'shake-128' | 'shake-256' | 'keccak-224' | 'keccak-256' | 'keccak-384' | 'keccak-512' | 'murmur3-128' | 'murmur3-32'
+const hash = (algorithm: HashAlgorithm) => async (data: Message): Promise<Buffer> => {
   switch (algorithm) {
     case 'sha3-224':
       return Buffer.from(sha3.sha3_224.arrayBuffer(data))
@@ -40,25 +41,22 @@ const hash = (algorithm) => async (data) => {
   }
 }
 
-const identity = data => Buffer.from(data)
+export const identity = (data: ArrayBuffer | SharedArrayBuffer): Buffer => Buffer.from(data)
 
-module.exports = {
-  identity,
-  sha1: sha('sha1'),
-  sha2256: sha('sha2-256'),
-  sha2512: sha('sha2-512'),
-  dblSha2256: sha('dbl-sha2-256'),
-  sha3224: hash('sha3-224'),
-  sha3256: hash('sha3-256'),
-  sha3384: hash('sha3-384'),
-  sha3512: hash('sha3-512'),
-  shake128: hash('shake-128'),
-  shake256: hash('shake-256'),
-  keccak224: hash('keccak-224'),
-  keccak256: hash('keccak-256'),
-  keccak384: hash('keccak-384'),
-  keccak512: hash('keccak-512'),
-  murmur3128: hash('murmur3-128'),
-  murmur332: hash('murmur3-32'),
-  addBlake: require('./blake')
-}
+export const sha1 = sha('sha1')
+export const sha2256 = sha('sha2-256')
+export const sha2512 = sha('sha2-512')
+export const dblSha2256 = sha('dbl-sha2-256')
+export const sha3224 = hash('sha3-224')
+export const sha3256 = hash('sha3-256')
+export const sha3384 = hash('sha3-384')
+export const sha3512 = hash('sha3-512')
+export const shake128 = hash('shake-128')
+export const shake256 = hash('shake-256')
+export const keccak224 = hash('keccak-224')
+export const keccak256 = hash('keccak-256')
+export const keccak384 = hash('keccak-384')
+export const keccak512 = hash('keccak-512')
+export const murmur3128 = hash('murmur3-128')
+export const murmur332 = hash('murmur3-32')
+export { default as addBlake } from './blake'
